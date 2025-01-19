76b83312ee7ab6c85d09cac0d7fa33524b339181
jeffng5 <jeffrey.ng51213@outlook.com>
2025-01-19 11:27:05 -0500

    modified queries to frontend

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.get('/', async (req, res, next) => {
 
     console.log('load route hit')
     try {
-        const results = await db.query(`SELECT DISTINCT recipes.id, recipes.name, image_url, images.recipe_id, COUNT(ratings.recipe_id) as divisor, sum(ratings.rating) as sumRating FROM recipes LEFT JOIN images ON images.recipe_id = recipes.id LEFT JOIN ratings ON images.recipe_id = ratings.recipe_id GROUP BY recipes.id,recipes.name, images.image_url, images.recipe_id ORDER BY recipes.id`);
-        console.log(results)
+        const results = await db.query(`SELECT DISTINCT recipes.id, recipes.name, image_url, images.recipe_id, COUNT(ratings.recipe_id) as divisor, sum(ratings.rating) as sumRating FROM recipes LEFT JOIN images ON images.recipe_id = recipes.id LEFT JOIN ratings ON images.recipe_id = ratings.recipe_id WHERE images.image_url IS NOT NULL GROUP BY recipes.id, recipes.name,images.image_url, images.recipe_id ORDER BY recipes.id`);
         return res.send(results)
     }
     catch (err) {
@@ -43,19 +42,19 @@ app.get('/tags', async function (req, res, next) {
         console.log('yes:', flattenedTags)
         try {
             const results = await db.query(`
+            SELECT table1.name, COUNT(table1.name), table1.image_url, table1.id, table1.divisor, table1.sumrating FROM 
             
-
-            SELECT table1.name, COUNT(table1.name), table1.image_url, table1.id FROM 
-            
-            (SELECT name, tag, recipes.id, images.image_url  
+            (SELECT name, tag, recipes.id, images.image_url, COUNT(ratings.recipe_id) as divisor, sum(ratings.rating) as sumrating  
                 FROM recipes 
                 JOIN tags on recipes.id = tags.recipe_id 
-                INNER JOIN images ON images.recipe_id = tags.recipe_id  
+                INNER JOIN images ON images.recipe_id = tags.recipe_id
+                LEFT JOIN ratings on ratings.recipe_id = recipes.id  
                 WHERE tag = $1 or tag = $2 or tag = $3 or tag = $4 or tag = $5 or tag = $6 or tag = $7 or tag = $8 or tag = $9 or tag = $10 or tag = $11 or tag = $12 or tag = $13 or tag =$14 or tag = $15 or tag = $16
-                ) as table1
-            GROUP BY name, image_url, table1.id
-            HAVING COUNT(name) >= 1`, [flattenedTags[0], flattenedTags[1], flattenedTags[2], flattenedTags[3], flattenedTags[4], flattenedTags[5], flattenedTags[6], flattenedTags[7], flattenedTags[8], flattenedTags[9], flattenedTags[10], flattenedTags[11], flattenedTags[12], flattenedTags[13], flattenedTags[14], flattenedTags[15]]);
-            console.log(results)
+                GROUP BY recipes.name, tags.tag, recipes.id, images.image_url) as table1
+            GROUP BY table1.name, table1.image_url, table1.id, table1.divisor, table1.sumrating
+            `, [flattenedTags[0], flattenedTags[1], flattenedTags[2], flattenedTags[3], flattenedTags[4], flattenedTags[5], flattenedTags[6], flattenedTags[7], flattenedTags[8], flattenedTags[9], flattenedTags[10], flattenedTags[11], flattenedTags[12], flattenedTags[13], flattenedTags[14], flattenedTags[15]]);
+            console.log(results.sumrating/results.divisor)
+
             return res.send(results)
         } catch (err) {
             console.log(err)
@@ -64,7 +63,7 @@ app.get('/tags', async function (req, res, next) {
     else {
         console.log('Test route')
         try {
-            const fallbackResults = await db.query(`SELECT DISTINCT recipes.id, recipes.name, image_url, images.recipe_id, COUNT(ratings.recipe_id) as divisor, sum(ratings.rating) as sumRating FROM recipes LEFT JOIN images ON images.recipe_id = recipes.id LEFT JOIN ratings ON images.recipe_id = ratings.recipe_id GROUP BY recipes.id,recipes.name, images.image_url, images.recipe_id ORDER BY recipes.id`)
+            const fallbackResults = await db.query(`SELECT DISTINCT recipes.id, recipes.name, image_url, images.recipe_id, COUNT(ratings.recipe_id) as divisor, sum(ratings.rating) as sumRating FROM recipes LEFT JOIN images ON images.recipe_id = recipes.id LEFT JOIN ratings ON images.recipe_id = ratings.recipe_id WHERE images.image_url IS NOT NULL GROUP BY recipes.id, recipes.name, images.image_url, images.recipe_id ORDER BY recipes.id`)
             return res.send(fallbackResults)
         } catch (err) {
             console.log(err)
@@ -116,11 +115,10 @@ app.get('/get-all-ratings', async function (req, res, next) {
     console.log('You have hit get-ratings route and it will be computed.')
 
     if (req.query.id) {
-    let id = req.query.id.id
+    let id = req.query.id
     console.log(req.query)
-  
     try {
-        const results = await db.query('SELECT id, COUNT(recipe_id) as divisor, sum(rating) as sumRating FROM ratings JOIN recipes ON recipes.id = ratings.recipe_id WHERE id = $1 GROUP BY id', [id]);
+        const results = await db.query('SELECT recipes.id, COUNT(recipe_id) as divisor, sum(rating) as sumRating FROM ratings JOIN recipes ON recipes.id = ratings.recipe_id WHERE recipes.id = $1 GROUP BY id', [id]);
 
         return res.send(results)
     } catch(err) {
@@ -130,6 +128,7 @@ app.get('/get-all-ratings', async function (req, res, next) {
 } else {
     return res.send('404 error')
 }
+
 })
 
 // app.get('get-all-ratings', async function (req, res, next) {
